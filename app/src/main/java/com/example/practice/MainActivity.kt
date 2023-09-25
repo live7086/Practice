6490f556e00c11967815377c667cdebdab2f137c
live7086 <luzzyyo@gmail.com>
2023-09-25 16:28:43 +0800

    Button done Card done LazyGrid done working on the mvp layout

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -3,21 +3,29 @@ package com.example.practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -43,8 +51,48 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Preview
+@Composable
+fun Content() {
+    Box (modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Surface (
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(5.dp)),
+            border = BorderStroke(width =5.dp, color = Color.LightGray)
+        ) {
+
+            Portfolio(data = listOf("Project1","Project2","Project3","Project3","Project3","Project3"))
+
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data){
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.swish2),
+                    contentDescription ="",
+                    modifier = Modifier.fillMaxWidth().height(200.dp))
+            }
+        }
+    }
+}
+
 @Composable
 fun CreateCard () {
+    val btnState = remember {
+        mutableStateOf(false)
+    }
     Surface (
         modifier = Modifier
             .fillMaxHeight()
@@ -57,8 +105,9 @@ fun CreateCard () {
                         .fillMaxWidth(1f)
                         .fillMaxHeight(0.5f)
                         .padding(10.dp),
+//                        .clickable {}
                     colors = CardDefaults.cardColors(containerColor = Color.LightGray),
-                    elevation = CardDefaults.cardElevation(10.dp),
+                    elevation = cardElevation(10.dp),
                 ) {
                     Image(painter = painterResource(
                         id = R.drawable.normal_failure),
@@ -67,13 +116,25 @@ fun CreateCard () {
                         contentScale = ContentScale.Crop,
                         )
                 }
+                Button(onClick = { btnState.value= !btnState.value }) {
+                    Text(text = "Click Me", style = androidx.compose.material.MaterialTheme.typography.button)
+                }
+
+                if(btnState.value){
+                        Content()
+                }else{
+                    Box {
+
+                    }
+                }
+
                 Card (
                     modifier = Modifier
                         .fillMaxWidth(1f)
                         .fillMaxHeight(1f)
                         .padding(10.dp),
                     colors = CardDefaults.cardColors(containerColor = Color.LightGray),
-                    elevation = CardDefaults.cardElevation(10.dp)
+                    elevation = cardElevation(10.dp)
                 ) {
 
                 }
@@ -83,7 +144,6 @@ fun CreateCard () {
 
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
